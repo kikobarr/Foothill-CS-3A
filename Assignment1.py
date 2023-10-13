c6b87ea6888d4c49dd41c835b1efa5bbfbf8fb28
Kiko Barr <34662575+kikobarr@users.noreply.github.com>
2023-10-12 18:48:47 -0700

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,30 @@
+"""
+Assignment 1: Opening Lines
+Submitted by Jennifer Barr
+Submitted:  September 29, 2023
+
+Assignment 1: This program demonstrates printing lines of text to the screen
+"""
+
+
+def print_header():
+    """Prints a header."""
+    print("STEM Center Temperature Project")
+    print("Jennifer Barr")
+
+
+def main():
+    """Calls the print header function"""
+    print_header()
+
+
+if __name__ == "__main__":
+    main()
+
+r"""
+/Users/kikobarr/PycharmProjects/Assignment1/venv/bin/python /Users/kikobarr/PycharmProjects/Assignment1/Assignment1.py 
+STEM Center Temperature Project
+Jennifer Barr
+
+Process finished with exit code 0
+"""
